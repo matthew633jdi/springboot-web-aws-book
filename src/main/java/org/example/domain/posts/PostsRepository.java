625473d691a72a27fe0b7a4f4ce500d19fc79f72
matthew633jdi <matthew633jdi@gmail.com>
2023-08-27 23:02:44 +0900

    mustache 추가 및 UI 개발

--- src/main/java/org/example/domain/posts/PostsRepository.java
+++ src/main/java/org/example/domain/posts/PostsRepository.java
@@ -1,8 +1,14 @@
 package org.example.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // iBatis, Mybatis 등에서 Dao라고 불리는 DB Layer 접근자
 // @Repository 생략 가능
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
